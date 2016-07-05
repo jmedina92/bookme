ff8e9f858a20c8b2fc567b3005a5af3e7abc3e11
jmedina92 <jmedina@wantudu.com>
2016-07-05 23:44:07 +0100

    Update post.js

--- resources/users/post.js
+++ resources/users/post.js
@@ -2,5 +2,7 @@ if (!(me && me.role == 'administrator')) {
     cancel("This is not administrator user", 401);
 }else if (this.role == 'administrator') {
     cancel("Can't create an administrator user", 401);
+}else if (!(this.role == 'limit' || this.role == 'consult')){
+    cancel("This is not a valid role", 401);
 }
 this.owner = me.id;
